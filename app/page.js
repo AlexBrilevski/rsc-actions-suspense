018c3e6e00ca5648ab070db7d002d70c2e01e632
Alex Brilevski <brilevski.alex@gmail.com>
2026-02-08 13:36:50 +0300

    Error handling was added

--- app/page.js
+++ app/page.js
@@ -5,20 +5,24 @@ import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UsePromiseDemo from "@/components/UsePromisesDemo";
+import ErrorBoundary from '@/components/ErrorBoundary';
 import { Suspense } from "react";
 
 export default function Home() {
-  const fetchedUsers = new Promise(resolve => setTimeout(async () => {
+  const fetchedUsers = new Promise((resolve, reject) => setTimeout(async () => {
     const data = await fs.readFile('dummy-db.json', 'utf-8');
     const users = JSON.parse(data);
     resolve(users);
+    // reject(new Error('Error!'));
   }, 3000));
 
   return (
     <main>
-      <Suspense fallback={<p>Loading data...</p>}>
-        <UsePromiseDemo usersPromise={fetchedUsers} />
-      </Suspense>
+      <ErrorBoundary fallback={'Something went wrong!'}>
+        <Suspense fallback={<p>Loading data...</p>}>
+          <UsePromiseDemo usersPromise={fetchedUsers} />
+        </Suspense>
+      </ErrorBoundary>
       <ClientDemo>
         <RSCDemo />
         <DataFetchingDemo />
